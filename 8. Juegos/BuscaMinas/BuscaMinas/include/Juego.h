70a935ae83baefdfce28e21fcf5a788fce3919a2
KevinRLE <klopeze14@miumg.edu.gt>
2025-03-23 22:32:37 -0600

    Actualización vidas BuscaMinas

---
+++ 8. Juegos/BuscaMinas/BuscaMinas/include/Juego.h
@@ -0,0 +1,25 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+#include "Tablero.h"
+
+class Juego
+{
+private:
+	Tablero tablero;
+	int cantidadMinas;
+
+	int aleatorio_en_rango(int minimo, int maximo);
+	int filaAleatoria();
+	int columnaAleatoria();
+	int vidas; //agregar
+public:
+    Juego(Tablero tablero, int cantidadMinas, int vidasTablero);
+	void colocarMinasAleatoriamente();
+	int solicitarFilaUsuario();
+	int solicitarColumnaUsuario();
+	bool jugadorGana();
+	void iniciar();
+	void dibujarPortada(string nombreArchivo);
+};
+
+#endif // JUEGO_H
